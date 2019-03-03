b8b3c82702e7279546be763ee07f2de2fa6b7008
Karel Fiala <fiala.karel@gmail.com>
2019-03-03 11:30:01 +0100

    Create useful dirs during the installation process

--- setup-full-auto.sh
+++ setup-full-auto.sh
@@ -102,6 +102,9 @@ runuser -l kfiala -c 'bash .dotfiles/create-symlinks.sh'
 
 # Minor tweak -- remove sudo message before first use
 runuser -l kfiala -c 'touch .sudo_as_admin_successful'
+runuser -l kfiala -c 'mkdir temp'
+runuser -l kfiala -c 'mkdir -p git/github.com'
+runuser -l kfiala -c 'mkdir -p git/gitlab.fialakarel.cz/kfiala'
 
 # Fix Keepass2 segfault on Ubuntu
 sed -i "s/cli/cli --verify-all/" $(which keepass2)
