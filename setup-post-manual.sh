44c64f15bb5d33ef765d4366a770148ba574bd48
Karel Fiala <fiala.karel@gmail.com>
2019-09-09 13:36:36 +0200

    Add spice-vdagent and gir1.2-spiceclientgtk-3.0 packages

--- setup-post-manual.sh
+++ setup-post-manual.sh
@@ -43,7 +43,8 @@ sudo apt-get -o Dpkg::Options::="--force-confold" install --yes \
     virt-goodies virt-manager vlan w3m wakeonlan wpasupplicant x2goclient xdotool \
     xinit xinput xtightvncviewer xorg zathura unzip parcellite acpi keepass2 \
     suckless-tools curl wget net-tools xbacklight xserver-xorg-video-intel \
-    mirage p7zip-full spice-vdagent gir1.2-spiceclientgtk-3.0 jq
+    mirage p7zip-full spice-vdagent gir1.2-spiceclientgtk-3.0 jq spice-vdagent \
+    gir1.2-spiceclientgtk-3.0
 
 # Fully upgrade
 sudo apt-get -o Dpkg::Options::="--force-confold" dist-upgrade -y
