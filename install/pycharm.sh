9a84431e73c8ee694351f7b4ce5d6e8236e1ba29
Karel Fiala <fiala.karel@gmail.com>
2019-11-17 13:08:44 +0100

    Add launcher

--- install/pycharm.sh
+++ install/pycharm.sh
@@ -4,8 +4,10 @@ set -v
 
 cd /tmp
 
+version="2019.2.4"
+
 # Get pycharm
-wget https://download.jetbrains.com/python/pycharm-community-2019.2.4.tar.gz -O pycharm.tar.gz
+wget https://download.jetbrains.com/python/pycharm-community-${version}.tar.gz -O pycharm.tar.gz
 
 # Extract
 sudo tar -xzf pycharm.tar.gz -C /opt/
@@ -18,3 +20,17 @@ cd /opt/pycharm-*/bin
 
 # Install pycharm
 /bin/sh pycharm.sh
+
+# Create launcher
+sudo tee /usr/share/applications/pycharm.desktop <<EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=PyCharm
+Icon=/opt/pycharm-community-${version}/bin/pycharm.png
+Exec=/opt/pycharm-community-${version}/bin/pycharm.sh
+Comment=The Drive to Develop
+Categories=Development;IDE;
+Terminal=false
+StartupWMClass=jetbrains-pycharm
+EOF
