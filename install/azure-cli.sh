e3ff397dce3775e5ebd55b36c467c858017118a5
Karel Fiala <fiala.karel@gmail.com>
2020-01-03 13:31:19 +0100

    Fix azure-cli.sh

--- install/azure-cli.sh
+++ install/azure-cli.sh
@@ -3,14 +3,14 @@
 set -v
 
 # Download and install the Microsoft signing key
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
-    gpg --dearmor | \
-    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+curl -sL https://packages.microsoft.com/keys/microsoft.asc \
+    | gpg --dearmor \
+    | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
 
 # Add the Azure CLI software repository
 AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" \
+    | sudo tee /etc/apt/sources.list.d/azure-cli.list
 
 # Update repository information and install the azure-cli package
 sudo apt-get update
