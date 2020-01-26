966d64cd746078754cace2bf46aff84aca0d33cc
Karel Fiala <fiala.karel@gmail.com>
2020-01-26 15:32:47 +0100

    Fix NUC sound noise

--- setup-specific-GESY610002XX.sh
+++ setup-specific-GESY610002XX.sh
@@ -6,3 +6,6 @@ set -v
 oldhostname=$(hostname)
 hostnamectl set-hostname beta
 sed -i "s/$oldhostname/beta/g" /etc/hosts
+
+# Fix NUC sound noise
+echo "options snd-hda-intel power_save=0 power_save_controller=N" >/etc/modprobe.d/alsa-base.conf
