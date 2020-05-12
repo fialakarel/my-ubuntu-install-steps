08ba54faaffc1f3b3f41a4cae853d27936d45955
Karel Fiala <fiala.karel@gmail.com>
2020-05-12 09:14:16 +0200

    Update helm version

--- install/helm.sh
+++ install/helm.sh
@@ -6,7 +6,7 @@ set -v
 mkdir $HOME/bin
 
 # Get Helm tgz
-wget https://storage.googleapis.com/kubernetes-helm/helm-v2.13.1-linux-amd64.tar.gz -O /tmp/helm.tgz
+wget https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz -O /tmp/helm.tgz
 
 # Extract helm binary
 tar -xzOf /tmp/helm.tgz linux-amd64/helm >$HOME/bin/helm
@@ -16,6 +16,3 @@ chmod +x $HOME/bin/helm
 
 # Cleaning
 rm /tmp/helm.tgz
-
-# Initialize helm
-helm init --client-only
