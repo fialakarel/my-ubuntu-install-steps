f67960e41ef6797a7ceacbd0c6c246210d823039
Karel Fiala <fiala.karel@gmail.com>
2020-06-03 20:50:42 +0200

    Update node version from v10 to v12 LTS.

--- install/node.sh
+++ install/node.sh
@@ -3,5 +3,5 @@
 set -v
 
 # Install Node.js
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
