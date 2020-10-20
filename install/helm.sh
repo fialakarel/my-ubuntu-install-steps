e6b5ca597fdcb21bcebcc62f63772f906c4ff7bf
Karel Fiala <fiala.karel@gmail.com>
2020-10-20 11:21:44 +0200

    Helm latest version

--- install/helm.sh
+++ install/helm.sh
@@ -5,8 +5,10 @@ set -v
 # Create dir for bin files
 mkdir $HOME/bin
 
+version="$(curl https://github.com/helm/helm/releases/latest | cut -d'"' -f2 | egrep -o "[0-9]+\.[0-9]+\.[0-9]+$")"
+
 # Get Helm tgz
-wget https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz -O /tmp/helm.tgz
+wget https://get.helm.sh/helm-v${version}-linux-amd64.tar.gz -O /tmp/helm.tgz
 
 # Extract helm binary
 tar -xzOf /tmp/helm.tgz linux-amd64/helm >$HOME/bin/helm
