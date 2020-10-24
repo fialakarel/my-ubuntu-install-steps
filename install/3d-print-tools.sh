a388d98be4162b1a5a8b56c35b5ff678ec6396b4
Karel Fiala <fiala.karel@gmail.com>
2020-10-24 11:12:50 +0200

    Replace slic3r with PrusaSlicer

--- install/3d-print-tools.sh
+++ install/3d-print-tools.sh
@@ -2,5 +2,41 @@
 
 set -v
 
+# OpenScad + unzip
 sudo apt update
-sudo apt install --yes slic3r wx-common libopengl-perl libwx-glcanvas-perl libwww-perl openscad
+sudo apt install --yes openscad unzip
+
+# PrusaSlicer
+tempdir="$(mktemp -d)"
+cd $tempdir
+user="$(id --user --name)"
+dst="/home/${user}/bin/PrusaSlicer"
+
+# Get it
+wget https://prusa3d.com/downloads/drivers/prusa3d_linux_2_2_9_1.zip -O prusa3d.zip
+
+# Unzip
+unzip prusa3d.zip
+
+# Install
+mv $(ls PrusaSlicer*.AppImage) $dst
+chmod +x $dst
+
+# Writing desktop file
+cat <<EOF | sudo tee /usr/share/applications/PrusaSlicer.desktop
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=PrusaSlicer
+Icon=PrusaSlicer
+Exec=${dst}
+Keywords=slice;3D;printer;convert;gcode;stl;obj;amf;
+StartupNotify=false
+StartupWMClass=PrusaSlicer
+MimeType=application/sla;model/x-wavefront-obj;model/x-geomview-off;application/x-amf;
+Categories=Development;Engineering;
+EOF
+
+# Cleaning
+cd 
+rm -rf $tempdir
