b0dcf26eeeb13e94d9a52f0ab0a8e5c03f99562c
Karel Fiala <fiala.karel@gmail.com>
2021-12-21 14:57:44 +0100

    Fix installation for Ubuntu 20.10

--- install/pyenv-poetry.sh
+++ install/pyenv-poetry.sh
@@ -4,7 +4,7 @@ set -ve
 
 # Install python build deps
 sudo apt update
-sudo apt install --yes bzip2 openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev gcc build-essential zlib1g zlibc zlib1g-dev
+sudo apt install --yes bzip2 openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev gcc build-essential zlib1g zlib1g-dev
 
 # Install pyenv
 curl https://pyenv.run | bash
@@ -13,7 +13,7 @@ curl https://pyenv.run | bash
 pyenv install 3.8.3
 
 # Install Poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
 
 # Install Poetry bash completion
 poetry completions bash | sudo tee /etc/bash_completion.d/poetry.bash-completion >/dev/null
