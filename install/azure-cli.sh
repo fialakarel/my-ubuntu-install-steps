d64d2bb024ce46e8e325302899e1136fc7c20292
Karel Fiala <fiala.karel@gmail.com>
2021-12-29 12:51:10 +0100

    Fix azure-cli installation

--- install/azure-cli.sh
+++ install/azure-cli.sh
@@ -2,19 +2,22 @@
 
 set -v
 
-# Download and install the Microsoft signing key
-curl -sL https://packages.microsoft.com/keys/microsoft.asc \
-    | gpg --dearmor \
-    | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+# option 1: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt#option-1-install-with-one-command
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-# Add the Azure CLI software repository
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" \
-    | sudo tee /etc/apt/sources.list.d/azure-cli.list
-
-# Update repository information and install the azure-cli package
-sudo apt-get update
-sudo apt-get install --yes azure-cli
-
-# Add Azure CLI bash completion
-sudo wget https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion -O /opt/az/az.completion
+## Download and install the Microsoft signing key
+#curl -sL https://packages.microsoft.com/keys/microsoft.asc \
+#    | gpg --dearmor \
+#    | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+#
+## Add the Azure CLI software repository
+#AZ_REPO=$(lsb_release -cs)
+#echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" \
+#    | sudo tee /etc/apt/sources.list.d/azure-cli.list
+#
+## Update repository information and install the azure-cli package
+#sudo apt-get update
+#sudo apt-get install --yes azure-cli
+#
+## Add Azure CLI bash completion
+#sudo wget https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion -O /opt/az/az.completion
